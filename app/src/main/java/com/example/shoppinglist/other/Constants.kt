d3a9f8c879dd5b9bd6cce93a512c68d58c47381f
Tochukwu <61085272+tochukwumunonye@users.noreply.github.com>
2020-10-04 00:26:12 +0100

    Update Constants.kt

--- app/src/main/java/com/example/shoppinglist/other/Constants.kt
+++ app/src/main/java/com/example/shoppinglist/other/Constants.kt
@@ -3,7 +3,7 @@ package com.example.shoppinglist.other
 class Constants{
 
     companion object{
-        const val API_KEY = "18495590-1fd8c29ad8143aaedfc5bc70e"
+        const val API_KEY = ""
         const val DATABASE_NAME = "shopping_db"
         const val BASE_URL = "https://pixabay.com"
         const val MAX_NAME_LENGTH = 20
